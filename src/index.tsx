d77ad869548376fc1e304894becbe4a7514f7590
Vithor Varela <vithor.v@upland.me>
2023-03-16 14:28:04 -0300

    feat: caching for performance

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { unpkgPathPlugin } from "./plugins/unpkg-path.plugin";
-
+import { fetchPlugin } from "./plugins/fetch-plugin";
 const App = () => {
   const ref = useRef<any>();
   const [input, setInput] = useState("");
@@ -11,7 +11,7 @@ const App = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "/esbuild.wasm",
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
   useEffect(() => {
@@ -27,7 +27,10 @@ const App = () => {
       entryPoints: ["index.js"],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [
+        unpkgPathPlugin(), 
+        fetchPlugin(input)
+      ],
       define: {
         "process.env.NODE_ENV": '"production"',
         global: "window",
